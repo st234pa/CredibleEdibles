acd0fe29275c72098da724bccc97d568c401e32c
st234pa <syoon1@stuy.edu>
2016-12-09 23:45:38 -0500

    Fixed key.py to get rid of newlines in the keys.

--- utils/key.py
+++ utils/key.py
@@ -1,5 +1,5 @@
 def getkeydict():
-    f = open("../keys.txt", r)
+    f = open("../keys.txt", 'r')
     listkeys = f.readlines()
     #-----List ordering-----
     #0. Yelp consumer_key
@@ -9,5 +9,12 @@ def getkeydict():
     #4. Mapbox access_token
     #5. Googlemaps key
     #-----------------------
+    for i in range(len(listkeys)):
+        x = len(listkeys[i])
+        listkeys[i] = listkeys[i][:x-1]
+
     d = {'yelp_consumer_key': listkeys[0], 'yelp_consumer_secret': listkeys[1], 'yelp_token': listkeys[2], 'yelp_token_secret': listkeys[3], 'mapbox_access_token': listkeys[4], 'googlemaps_key': listkeys[5]}
+    #print d.values()
     return d
+    
+getkeydict()
