672a88f9844637a417e729123b0ecc9e831b229f
st234pa <syoon1@stuy.edu>
2016-12-09 23:52:53 -0500

    closed file in key.py

--- utils/key.py
+++ utils/key.py
@@ -1,6 +1,7 @@
 def getkeydict():
     f = open("../keys.txt", 'r')
     listkeys = f.readlines()
+    f.close()
     #-----List ordering-----
     #0. Yelp consumer_key
     #1. Yelp consumer_secret
@@ -14,7 +15,7 @@ def getkeydict():
         listkeys[i] = listkeys[i][:x-1]
 
     d = {'yelp_consumer_key': listkeys[0], 'yelp_consumer_secret': listkeys[1], 'yelp_token': listkeys[2], 'yelp_token_secret': listkeys[3], 'mapbox_access_token': listkeys[4], 'googlemaps_key': listkeys[5]}
-    #print d.values()
+    print d.values()
     return d
     
 getkeydict()
