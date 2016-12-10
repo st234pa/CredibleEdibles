c38dc4c6b8c6d4d2011bf4ffe1870db479a760b6
st234pa <syoon1@stuy.edu>
2016-12-09 23:18:41 -0500

    Wrote key.py to create a dictionary of keys/tokens and stuff of that nature. Other python files should import key so they can create that dictionary.

---
+++ utils/key.py
@@ -0,0 +1,13 @@
+def getkeydict():
+    f = open("keys.txt", r)
+    listkeys = f.readlines()
+    #-----List ordering-----
+    #0. Yelp consumer_key
+    #1. Yelp consumer_secret
+    #2. Yelp token
+    #3. Yelp token_secret
+    #4. Mapbox access_token
+    #5. Googlemaps key
+    #-----------------------
+    d = {'yelp_consumer_key': listkeys[0], 'yelp_consumer_secret': listkeys[1], 'yelp_token': listkeys[2], 'yelp_token_secret': listkeys[3], 'mapbox_access_token': listkeys[4], 'googlemaps_key': listkeys[5]}
+    return d
