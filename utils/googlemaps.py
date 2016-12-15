b73b4500dfab0d7f393e361b756f7329b9a3b2e6
Lorenz Vargas <lvargas@stuy.edu>
2016-12-15 03:51:02 -0500

    don't know what happened here

--- utils/googlemaps.py
+++ utils/googlemaps.py
@@ -1,56 +1,25 @@
 import cgi
 import urllib2
+import urllib
 import key
 
-#import json, requests
-import json
+import json #requests > urllib2 any day of the week
 
-
-#urllib2 
-#	.urlopen
-#	u = urllib2.urlopen(<URL>)
-#	open a url to be read by your program
-#	.geturl()
-#		reutnrs the actual url (in case of redirects)
-#	.info()
-#		returns the http/s header information
-#	.read()
-#		returns the contents of the target webpage.
-#	use urlopen on ur rest api links
-#	now its going to grab what I would get from this website
-#	should return some json object as a string
-#	json:
-#		library to work with json data.
-#		.loads
-#			d = json.loads(<STRING>)
-#		.dumps(<DICTIONARY>)
-#			turns a python dict into a json object string
-
-
-		
-#API functions
-
-# This prepares the credentials for Mapbox
-#takes an address and returns the coordinates
-
-def geocode(address):
+def locate():
 	token = ""
-	token = key.getkeydict()['mapbox_access_token']
-	query = ""
-	query = address.replace(" ", "%20")
-	url = "https://api.mapbox.com/geocoding/v5/mapbox.places/\"" + query + "\".json?access_token=" + token
-	response = urllib2.urlopen(url)
-	data = response.read()
-	dic = json.loads(data)
-	return dic['features'][0]['geometry']['coordinates']
-
-	##~~~~~~~~OLD stuff WITH REQUESTS~~~~~~~~~~~~~##
-	#url for accessing mapbox api
-	#url = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + address + ".json?access_token=pk.eyJ1IjoibGF1cmVudGl1czkxMiIsImEiOiJjaXdkd2k2cnIwZDJ0MnpwZmViaWZ4OW9lIn0.eXZa4xOeNmH_Zuw-MukY9g"
+	token = key.getkeydict()['googlemaps_key']
+	url = "https://www.googleapis.com/geolocation/v1/geolocate?key=" + token
+	query_args = {}
+	#url for accessing google maps geolcation api
+	data = urllib.urlencode(query_args)
 	#gets page info
-	#data = requests.get(url).text
+	request = urllib2.Request(url, data)
+	response = urllib2.urlopen(request)
+	stuff = response.read()
 	#encodes into utf
-	#response = data.encode('utf8')
 	#turns into dictionary
-	#dic = json.loads(response)
-	#return dic['features'][0]['geometry']['coordinates']
+	dic = json.loads(stuff)
+	loc = []
+	loc.append(float(unicode(dic['location']['lat']).encode('utf-8')))
+	loc.append(float(unicode(dic['location']['lng']).encode('utf-8')))
+	return loc
